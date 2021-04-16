f4b3fe8362fcbf321b199a39d792194c7c3f51b6
claire ogesen <claire.ogesen.co@gmail.com>
2021-04-16 13:26:11 -0700

    added 1st querie

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,26 @@ const app = express();
 //express middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      //  MySQL username,
+      user: 'root',
+      //  MySQL password
+      password: 'Root',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+      console.log(rows);
+  });
+
 
 // Below is a good way to test to make sure express is working 
 //should say hello world on localhost:3001
@@ -16,7 +37,7 @@ app.use(express.json());
 //   });
 
 //Default response for any other request (Not Found) 
-//this is a catch all and needs to be at the end or it over rides all 
+// this is a catch all and needs to be at the end or it over rides all 
 app.use((req, res) => {
     res.status(404).end();
   });
